1377b43a7d10b258869b004b8f383fc13caa85e5
BreakZero <dejin.po@gmail.com>
2023-09-05 09:57:30 +0800

    init commit

---
+++ settings.gradle.kts
@@ -0,0 +1,24 @@
+pluginManagement {
+    includeBuild("build-logic")
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+    versionCatalogs {
+        create("libs") {
+            from(files("./build-logic/libs.versions.toml"))
+        }
+    }
+}
+
+rootProject.name = "BuildLogicExample"
+include(":app")
+ 
